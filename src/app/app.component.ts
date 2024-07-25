2976cfa6ce7a74d63f1b823de63dbbdf46f1a349
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-07-25 07:00:32 +0300

    html markup and basic css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'USENSE_Task';
+  passwordField = new FormControl('', [
+    Validators.required,
+    Validators.minLength(8),
+  ]);
 }
